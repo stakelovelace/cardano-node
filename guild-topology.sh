0f0ab49583ac02a7ed3be2cd63c2743c9c10e19c
stakelovelace <56354546+stakelovelace@users.noreply.github.com>
2020-08-22 15:03:38 +0200

    Update guild-topology.sh

--- guild-topology.sh
+++ guild-topology.sh
@@ -7,7 +7,7 @@ curl -s -k -o /tmp/guild_topology2.json "https://api.clio.one/htopology/v1/fetch
 cat /tmp/guild_topology2.json | awk '{print $3,$5}' | tail -n +2 | sed s/"\","//g  | sed s/"\""//g | sed s/","//g | grep -v [a-z] >  /tmp/guild_list1              
 
 IFS=$'\n'; for i in $(cat /tmp/guild_list1 ); do sudo tcpping -x 1 $i | grep ms | awk '{print $9,$7}' >> /tmp/guild_list2 ; done
-cat /tmp/guild_list2 | sort -n | grep -v "ms" | head -n 5 | cut -d "(" -f 2 | cut -d ")" -f 1   > /tmp/fastest_guild.list
+cat /tmp/guild_list2 | sort -n | grep -v "ms" | head -n 8 | cut -d "(" -f 2 | cut -d ")" -f 1   > /tmp/fastest_guild.list
  
 IFS=$'\n'; for i in $(cat /tmp/fastest_guild.list); do  cat /tmp/guild_list1 | grep "$i" >> /tmp/guild_list3; done
 
@@ -22,6 +22,12 @@ AADD4=$(sed -n 4p /tmp/guild_list3 | awk '{print $1}')
 AADD4PORT=$(sed -n 4p /tmp/guild_list3 | awk '{print $2}')
 AADD5=$(sed -n 5p /tmp/guild_list3 | awk '{print $1}')
 AADD5PORT=$(sed -n 5p /tmp/guild_list3 | awk '{print $2}')
+AADD6=$(sed -n 6p /tmp/guild_list3 | awk '{print $1}')
+AADD6PORT=$(sed -n 6p /tmp/guild_list3 | awk '{print $2}')
+AADD7=$(sed -n 7p /tmp/guild_list3 | awk '{print $1}')
+AADD7PORT=$(sed -n 7p /tmp/guild_list3 | awk '{print $2}')
+AADD8=$(sed -n 8p /tmp/guild_list3 | awk '{print $1}')
+AADD8PORT=$(sed -n 8p /tmp/guild_list3 | awk '{print $2}')
 
 cat <<EOF > $CNODE_HOME/priv/files/guild_topology.json
 { "resultcode": "201", "networkMagic": "764824073", "ipType":4, "Producers": [
@@ -33,9 +39,12 @@ cat <<EOF > $CNODE_HOME/priv/files/guild_topology.json
   { "addr": "$AADD3", "port": $AADD3PORT, "valency": 1, "distance":10 },
   { "addr": "$AADD4", "port": $AADD4PORT, "valency": 1, "distance":10 },
   { "addr": "$AADD5", "port": $AADD5PORT, "valency": 1, "distance":10 },
-  { "addr": "78.47.99.41", "port": 6000, "valency": 1, "distance":10 },
-  { "addr": "168.119.51.182", "port": 6000, "valency": 1, "distance":10 },
-  { "addr": "159.69.185.211", "port": 6000, "valency": 1, "distance":10 }
+  { "addr": "$AADD6", "port": $AADD6PORT, "valency": 1, "distance":10 },
+  { "addr": "$AADD7", "port": $AADD7PORT, "valency": 1, "distance":10 },
+  { "addr": "$AADD8", "port": $AADD8PORT, "valency": 1, "distance":10 },
+  { "addr": "78.47.99.41", "port": 6000, "valency": 2, "distance":10 },
+  { "addr": "168.119.51.182", "port": 6000, "valency": 2, "distance":10 },
+  { "addr": "159.69.185.211", "port": 6000, "valency": 2, "distance":10 }
 ] }
 EOF
 
