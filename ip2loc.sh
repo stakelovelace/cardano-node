78a2060fe809fd18e11d118d1d1910e9594e52cc
stakelovelace <56354546+stakelovelace@users.noreply.github.com>
2020-08-23 22:55:12 +0200

    Update ip2loc.sh

--- ip2loc.sh
+++ ip2loc.sh
@@ -2,8 +2,8 @@
  
 export CNODE_HOME=/opt/cardano/cnode
 
-touch /tmp/ip2trace_in.log && rm /tmp/ip2trace_in.log > /dev/null 2>&1
-touch /tmp/ip2trace_out.log && rm /tmp/ip2trace_out.log > /dev/null 2>&1
+truncate -s 0  /tmp/ip2trace_out.log;
+truncate -s 0  /tmp/ip2trace_in.log;
 
 pHOST=$HOSTNAME
 pIP=$(ifconfig eth0  | grep inet | awk '{print $2}')
@@ -15,9 +15,9 @@ netstat -nt  | grep tcp | grep EST | grep -v "$pIP:$pPORT" | awk '{ print $5 }' 
 netstat -nt  | grep tcp | grep EST | grep -v "$pIP:$pPORT" | awk '{ print $5 }' | cut -d ':' -f 1 | grep 172 > /tmp/iptrace_list_out_local.csv
 sleep 3 2>&1; 
 
-/usr/local/bin/ip2location -list /tmp/iptrace_list_in.csv -t all > /tmp/ip2trace_list_in.plog > /dev/null 2>&1
+/usr/local/bin/ip2location -list /tmp/iptrace_list_in.csv -t all > /tmp/ip2trace_list_in.plog
 sleep 2;
-/usr/local/bin/ip2location -list /tmp/iptrace_list_out.csv -t all > /tmp/ip2trace_list_out.plog > /dev/null 2>&1
+/usr/local/bin/ip2location -list /tmp/iptrace_list_out.csv -t all > /tmp/ip2trace_list_out.plog
 
 
 LinesIN=$(cat /tmp/ip2trace_list_in.plog | wc -l)
@@ -26,4 +26,3 @@ timestamp=$(date +%D)
 time=$(date +%T)
 for ((i=1;i<=$LinesIN;i++)); do ADD=$(sed -n "$i"p /tmp/ip2trace_list_in.plog); echo "timestamp=$timestamp,time=$time,pHOST=$pHOST,pIP=$pIP,pPORT=$pPORT,app=$ADD" | sed s/" country_long"/",country_long"/g | sed s/" "/"_"/g | sed s/","/" "/g | sed s/"\""/""/g >> /tmp/ip2trace_in.log; done
 for ((i=1;i<=$LinesOUT;i++)); do ADD=$(sed -n "$i"p /tmp/ip2trace_list_out.plog); echo "timestamp=$timestamp,time=$time,pHOST=$pHOST,pIP=$pIP,pPORT=$pPORT,app=$ADD" | sed s/" country_long"/",country_long"/g | sed s/" "/"_"/g | sed s/","/" "/g | sed s/"\""/""/g >> /tmp/ip2trace_out.log; done
-
