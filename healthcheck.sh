3276632c7e9ec0a69355e67df8566257eb0b9f6d
stakelovelace <56354546+stakelovelace@users.noreply.github.com>
2020-12-12 03:07:32 +0100

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 source /opt/cardano/cnode/scripts/env
+
 CCLI=$(which cardano-cli)
 
-FIRST=$($CCLI query tip --shelley-mode --mainnet | jq .blockNo)
+FIRST=$($CCLI query tip --shelley-mode --testnet-magic $(grep Magic /opt/cardano/cnode/priv/files/$NETWORK-shelley-genesis.json | awk '{ print $2 }' | cut -d "," -f 1) | jq .blockNo)
 
 sleep 60;
 
-SECOND=$($CCLI query tip --shelley-mode --mainnet | jq .blockNo)
+SECOND=$($CCLI query tip --shelley-mode --testnet-magic $(grep Magic /opt/cardano/cnode/priv/files/$NETWORK-shelley-genesis.json | awk '{ print $2 }' | cut -d "," -f 1) | jq .blockNo)
 
 
 if [[ "$FIRST" -ge "$SECOND" ]]; then
